9210f6bac5e98165d14a6a83eacffaed989a6240
roxy0507 <roxy.0507@gmail.com>
2021-03-29 12:35:28 -0600

    add order and user models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admins
-  root to: "home#index"
+  authenticate :admin do 
+    resources :products
+    resources :categories
+  end
+  #root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed}
+#la clase se llama dir. Fail.join= nos permite unir las carpetas de db y seets y nos une la ruta. Nos entrega un array. *rb cualquier archivo que sea .rb
+# la ruta 'db', 'seeds', '*.rb' dentro de la carpeta seeds estan los archivos rb, esto sera el array que nos entregue, con todos los rb. 
+#.sort nos ayuda a ordenar alfabeticamente. 
+#.each carga este archivo
